d3dd7564bd46b063b8bb34750cb24d47853b47ca
han190 <limpid.mazarine@gmail.com>
2020-05-18 01:17:42 -0800

    update euler_main.f90

--- src/euler_main.f90
+++ src/euler_main.f90
@@ -4,49 +4,89 @@ program main
     use euler_prob_api_m
     implicit none 
 
-    integer, parameter :: nop = 58
-    character(len=20) :: ans(nop)
-    character(len=1), parameter :: space = " ", dash = "-"
-    character(len=20), parameter :: failed = repeat(space, 19)//"x"
-    character(len=7), parameter :: center_aligned = "|:"//repeat(dash, 4)//":"
-    real :: t_f, t_i, tspan(nop), tsum = 0., nslv = 0.
-    type(euler_probs_t) :: probs(nop)
-    integer :: i
+    integer :: arg_num, arg_len, arg_stat
+    character(len=50) :: arg_val(0:2)
 
-    call euler_init(probs)
+    if (command_argument_count() > 4) then
+        print "(a)", "SYNTAX ERROR: -h or --help for further information."
+        stop
+    end if
 
-    open(47, file = "ANSWER.md")
-    write (47, "(a)") "# Project Euler with Modern Fortran"//new_line("a")
-    write (47, "(a)") "## Compilers"//new_line("a")
-    write (47, "(a)") compiler_version()//new_line("a")
-    write (47, "(a)") "## Answers and Benchmarks"//new_line("a")
-    write (47, "(a)") "|Prob|Answer|Tspan(s)|T/Ttot(%)|"
-    write (47, "(a)") repeat(center_aligned, 4)//"|"
+    arg_num = 0
+    read_argument_loop: do
+        call get_command_argument(arg_num, arg_val(arg_num), arg_len, arg_stat)
+        if (len_trim(arg_val(arg_num)) == 0 .or. arg_num >= 3) then
+            exit read_argument_loop
+        end if
+        arg_num = arg_num + 1
+    end do read_argument_loop
 
-    do i = 1, nop
-        call cpu_time(t_i)
-        ans(i) = probs(i)%ans()
-        call cpu_time(t_f)
-        tspan(i) = t_f - t_i
-    end do
+    select case(arg_val(1))
+    case("-h", "--help")
+        call get_help()
+        stop
+    case("-c", "--compute-all-problems")
+        call compute_all_problems()
+    case default
+        print "(a)", "SYNTAX ERROR: -h or --help for further information."
+        stop
+    end select
 
-    tsum = sum(tspan, dim = 1)
-    nslv = real( count(ans /= failed, dim = 1) )
+contains
 
-    do i = 1, nop
-        write (47, 1120) "|", i, "|"//ans(i)//"|", tspan(i), &
-            "|", tspan(i) / tsum * 100., "%|"
-    end do
+    subroutine get_help()
+        print "(a)", "Project Euler with Modern Fortran"
+        print "(a)", "Syntax:"
+        print "(a)", "./Euler -h/--help"
+        print "(a)", "./Euler -c/--compute-all-problems"
+    end subroutine get_help
 
-    write (47, "(a)") new_line("a")//"## Summary"//new_line("a")
-    write (47, "(a)") "|Benchmarks|Results|"
-    write (47, "(a)") repeat(center_aligned, 2)//"|"
-    write (47, "(a, i4, a)") "|Problems solved|", int(nslv), "|"
-    write (47, 1121) "|Total time spent|", tsum, "(s)|"
-    write (47, 1121) "|Average time spent per problem| ", tsum/nslv, "(s)|"
+    subroutine compute_all_problems()
+        integer, parameter :: nop = 58
+        character(len=20) :: ans(nop)
+        character(len=1), parameter :: space = " ", dash = "-"
+        character(len=20), parameter :: failed = repeat(space, 19)//"x"
+        character(len=7), parameter :: c_aligned = "|:"//repeat(dash, 4)//":"
+        real :: t_f, t_i, tspan(nop), tsum = 0., nslv = 0.
 
-    close(47)
+        type(euler_probs_t) :: probs(nop)
+        integer :: i
 
-1120 format(a, i6, a22, f10.6, a, f9.4, a)
-1121 format(a, f10.6, a)
+        call euler_init(probs)
+
+        open(1, file = "ANSWER.md")
+        write (1, "(a)") "# Project Euler with Modern Fortran"//new_line("a")
+        write (1, "(a)") "## Compilers"//new_line("a")
+        write (1, "(a)") compiler_version()//new_line("a")
+        write (1, "(a)") "## Answers and Benchmarks"//new_line("a")
+        write (1, "(a)") "|Prob|Answer|Tspan(s)|T/Ttot(%)|"
+        write (1, "(a)") repeat(c_aligned, 4)//"|"
+
+        do i = 1, nop
+            call cpu_time(t_i)
+            ans(i) = probs(i)%ans()
+            call cpu_time(t_f)
+            tspan(i) = t_f - t_i
+        end do
+
+        tsum = sum(tspan, dim = 1)
+        nslv = real(count(ans /= failed, dim = 1))
+
+        do i = 1, nop
+            write (1, 1120) i, ans(i), tspan(i), tspan(i) / tsum * 100.
+            1120 format("|", i6, "|", a20, "|", f10.6, "|", f9.4, "%|")
+        end do
+
+        write (1, "(a)") new_line("a")//"## Summary"//new_line("a")
+        write (1, "(a)") "|Benchmarks|Results|"
+        write (1, "(a)") repeat(c_aligned, 2)//"|"
+        write (1, 1121) int(nslv)
+        1121 format("|Problems solved|", i4, "|")
+        write (1, 1122) "|Total time spent|", tsum, "(s)|"
+        write (1, 1122) "|Average time spent per problem|", tsum/nslv, "(s)|"
+        1122 format(a, f10.6, a)
+
+        close(1)
+    end subroutine compute_all_problems
+
 end program main 
