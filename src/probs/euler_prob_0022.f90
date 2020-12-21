9fa61ba030bcd5566d574bf4d709e1c70d3fe79a
han190 <limpid.mazarine@gmail.com>
2020-12-21 12:53:27 -0900

    update euler_prob_0022.f90; update euler_lexical_sort.f90

--- src/probs/euler_prob_0022.f90
+++ src/probs/euler_prob_0022.f90
@@ -7,39 +7,34 @@ contains
         write (euler0022, "(i20)") ans()
     end function euler0022
 
-    integer(int64) function ans()
+    integer function ans()
         use euler_lexical_sort_m, only: lexical_sort
         implicit none
-        integer(int64) :: i, j, stat, tmp
-        character(len=20), allocatable :: arr_of_names(:)
+        integer :: i, j, istat, tmp
+        character(len=:), allocatable :: names(:)
 
         open (unit=22, file="euler0022.txt", status="old", action="read")
-        allocate(arr_of_names(6000))
-        arr_of_names = "n/a"
-        read (22, *, iostat=stat) arr_of_names(1:6000)
+        allocate(character(len=20) :: names(6000))
+        names = "n/a"
+        read (22, *, iostat=istat) names
 
-        i = 1_int64
-        do while (arr_of_names(i) /= "n/a")
-            i = i + 1_int64
-        end do
-        i = i - 1_int64
-
-        call lexical_sort(arr_of_names(1:i))
-        tmp = 0_int64
-        do j = 1_int64, i
-            tmp = tmp + j*score_of_letters(arr_of_names(j))
+        i = count(names /= "n/a")
+        call lexical_sort(names(1:i))
+        tmp = 0
+        do j = 1, i
+            tmp = tmp + j*score_of_letters(names(j))
         end do
 
         ans = tmp
     end function ans
 
-    integer(int64) function score_of_letters(str)
+    integer function score_of_letters(str)
         character(*), intent(in) :: str
-        integer(int64) :: j, s
+        integer :: j, s
 
-        s = 0_int64
-        do j = 1_int64, len_trim(str)
-            s = s + iachar(str(j:j)) - 64_int64
+        s = 0
+        do j = 1, len_trim(str)
+            s = s + iachar(str(j:j)) - 64
         end do
         score_of_letters = s
     end function score_of_letters
