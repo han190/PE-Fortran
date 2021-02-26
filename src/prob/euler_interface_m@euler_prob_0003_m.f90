a64c886cf673d91939b0cf2e29c80000813088d9
han190 <limpid.mazarine@gmail.com>
2021-02-26 08:54:56 -0900

    rewrite function answer

--- src/prob/euler_interface_m@euler_prob_0003_m.f90
+++ src/prob/euler_interface_m@euler_prob_0003_m.f90
@@ -4,21 +4,36 @@ submodule(euler_interface_m) euler_prob_0003_m
 contains
 
     module character(len=20) function euler0003()
-        write (euler0003, "(i20)") ans(600851475143_int64)
+        write (euler0003, "(i20)") answer(600851475143_int64)
     end function euler0003
 
-    integer(int64) function ans(n)
+    function answer(n) result(ret)
         use euler_primes_m, only: sieve_of_Eratosthenes
         implicit none
+
         integer(int64), intent(in) :: n
         logical, allocatable :: is_prime(:)
-        integer(int64) :: fmax, i
+        integer(int64) :: max_, ret
 
-        fmax = int(sqrt(real(n, sp)) + 1.0_sp, int64)
-        call sieve_of_Eratosthenes(fmax, is_prime)
-        do i = fmax, 2_int64, -1_int64
-            if (is_prime(i) .and. mod(n, i) == 0_int64) exit
+        max_ = int(sqrt(real(n, sp)) + 1._sp, int64)
+        call sieve_of_Eratosthenes(max_, is_prime)
+        do ret = max_, 2_int64, -1_int64
+            if (is_prime(ret) .and. mod(n, ret) == 0_int64) exit
         end do
-        ans = i
-    end function ans
+    end function answer
+
+    ! integer(int64) function ans(n)
+    !     use euler_primes_m, only: sieve_of_Eratosthenes
+    !     implicit none
+    !     integer(int64), intent(in) :: n
+    !     logical, allocatable :: is_prime(:)
+    !     integer(int64) :: fmax, i
+
+    !     fmax = int(sqrt(real(n, sp)) + 1.0_sp, int64)
+    !     call sieve_of_Eratosthenes(fmax, is_prime)
+    !     do i = fmax, 2_int64, -1_int64
+    !         if (is_prime(i) .and. mod(n, i) == 0_int64) exit
+    !     end do
+    !     ans = i
+    ! end function ans
 end submodule euler_prob_0003_m
