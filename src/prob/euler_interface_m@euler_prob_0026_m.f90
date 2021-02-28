51cb7201201a83b78d78e45ff3089f8d852e3a30
han190 <limpid.mazarine@gmail.com>
2021-02-27 17:18:09 -0900

    Delete function ans.

--- src/prob/euler_interface_m@euler_prob_0026_m.f90
+++ src/prob/euler_interface_m@euler_prob_0026_m.f90
@@ -28,27 +28,6 @@ contains
         end do
     end function answer
 
-    ! integer(int64) function ans(n)
-    !     use euler_primes_m, only: sieve_of_Eratosthenes
-    !     implicit none
-    !     integer(int64), intent(in) :: n
-    !     integer(int64) :: tmp, k, ki, i
-    !     logical, allocatable :: is_prime(:)
-
-    !     call sieve_of_Eratosthenes(n, is_prime)
-    !     tmp = 0_int64; k = 1_int64
-    !     do i = 7_int64, n
-    !         if (is_prime(i)) then
-    !             tmp = multiplicative_order(i, 10_int64)
-    !             if (tmp > k) then
-    !                 k = tmp
-    !                 ki = i
-    !             end if
-    !         end if
-    !     end do
-    !     ans = ki
-    ! end function ans
-
     integer(int64) function multiplicative_order(n, m)
         integer(int64), intent(in) :: n, m
         integer(int64) :: res, k, tmp
