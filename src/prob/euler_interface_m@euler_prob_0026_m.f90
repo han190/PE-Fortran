bec11f7ff796a687f3dd202135f9191160aee0f7
han190 <limpid.mazarine@gmail.com>
2021-02-27 17:14:34 -0900

    Fix 'uninitialized ret' issue.

--- src/prob/euler_interface_m@euler_prob_0026_m.f90
+++ src/prob/euler_interface_m@euler_prob_0026_m.f90
@@ -4,29 +4,50 @@ submodule(euler_interface_m) euler_prob_0026_m
 contains
 
     module character(len=20) function euler0026()
-        write (euler0026, "(i20)") ans(1000_int64)
+        write (euler0026, "(i20)") answer(1000_int64)
     end function euler0026
 
-    integer(int64) function ans(n)
+    function answer(n) result(ret)
         use euler_primes_m, only: sieve_of_Eratosthenes
         implicit none
+
         integer(int64), intent(in) :: n
-        integer(int64) :: tmp, k, ki, i
+        integer(int64) :: ret, tmp, k, i
         logical, allocatable :: is_prime(:)
 
         call sieve_of_Eratosthenes(n, is_prime)
-        tmp = 0_int64; k = 1_int64
+        tmp = 0_int64; k = 1_int64; ret = 0_int64
         do i = 7_int64, n
             if (is_prime(i)) then
                 tmp = multiplicative_order(i, 10_int64)
                 if (tmp > k) then
                     k = tmp
-                    ki = i
+                    ret = i
                 end if
             end if
         end do
-        ans = ki
-    end function ans
+    end function answer
+
+    ! integer(int64) function ans(n)
+    !     use euler_primes_m, only: sieve_of_Eratosthenes
+    !     implicit none
+    !     integer(int64), intent(in) :: n
+    !     integer(int64) :: tmp, k, ki, i
+    !     logical, allocatable :: is_prime(:)
+
+    !     call sieve_of_Eratosthenes(n, is_prime)
+    !     tmp = 0_int64; k = 1_int64
+    !     do i = 7_int64, n
+    !         if (is_prime(i)) then
+    !             tmp = multiplicative_order(i, 10_int64)
+    !             if (tmp > k) then
+    !                 k = tmp
+    !                 ki = i
+    !             end if
+    !         end if
+    !     end do
+    !     ans = ki
+    ! end function ans
 
     integer(int64) function multiplicative_order(n, m)
         integer(int64), intent(in) :: n, m
