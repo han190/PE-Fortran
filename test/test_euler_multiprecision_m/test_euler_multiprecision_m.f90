a2bf591074436a771dbf845459add35ee5f4a7a7
han190 <limpid.mazarine@gmail.com>
2021-08-11 15:46:43 -0800

    edit test messages

--- test/test_euler_multiprecision_m/test_euler_multiprecision_m.f90
+++ test/test_euler_multiprecision_m/test_euler_multiprecision_m.f90
@@ -5,50 +5,75 @@ program main
 
     type(multiprecision_int_t) :: a, b, c
     integer :: p
+    logical :: cond
 
-    print "(a)", new_line("a")//"Test multiple precision integer type"
+    call title("'euler_multiprecision_m'")
     test_addition: block
         a = "23459872394587023948572394857923457923458723455432"
         b = "23409587239485723489572934857293450234985723457632"
-        c = a + b
-
-        print "(a)", "(a) addition"
-        print "(a, a1, 50(i1))", "a         = ", a%sgn, a%arr
-        print "(a, a1, 50(i1))", "b         = ", b%sgn, b%arr
-        print "(a, a1, 50(i1))", "c = a + b = ", c%sgn, c%arr
+        c = "46869459634072747438145329715216908158444446913064"
+        cond = c == a + b
+        
+        call test(cond, "addition")
     end block test_addition
 
     test_subtraction: block
         a = "23459872394587023948572394857923457923458723455432"
         b = "23409587239485723489572934857293450234985723457632"
-        c = a - b
+        c = "-99949714844898699541000539999369992311527000002200"
+        cond = c == a - b
 
-        print "(a)", "(b) subtraction"
-        print "(a, a1, 50(i1))", "a         = ", a%sgn, a%arr
-        print "(a, a1, 50(i1))", "b         = ", b%sgn, b%arr
-        print "(a, a1, 50(i1))", "c = a - b = ", c%sgn, c%arr
+        call test(cond, "subtraction")
     end block test_subtraction
 
     test_multiplication: block
         a = "23459872394587023948572394857923457923458723455432"
         b = "23409587239485723489572934857293450234985723457632"
-        c = a*b
+        c = "54918592944828777958592229956265660805018656206857"//&
+            "9912143134513601935683616475208660009329692257024"
+        cond = c == a*b
 
-        print "(a)", "(c) multiplication"
-        print "(a, a1, 50(i1))", "a         = ", a%sgn, a%arr
-        print "(a, a1, 50(i1))", "b         = ", b%sgn, b%arr
-        print "(a, a1, 50(i1))", "c = a * b = ", c%sgn, c%arr
+        call test(cond, "multiplication")
     end block test_multiplication
 
     test_power: block
         a = "23459872394587023948572394857923457923458723455432"
         p = 12
-        c = a**p
+        c = "27791228582771098796131351564405163521247196723249"//&
+            "87223036707220745161360119522300106027135557338905"//&
+            "65935311089076019480759260891856804808115185407046"//&
+            "08649190490054254144287397101266811250646076901754"//&
+            "24181773116649253700400781790669870235099026247270"//&
+            "28018630205511731061179129346558847567952448848956"//&
+            "60492659285963273856756812620640739475076221647126"//&
+            "87045544298274187571179193061352556073368908863300"//&
+            "94368159857188317292217489483960010604983574558711"//&
+            "47845192831878612791307575860211356222986497999320"//&
+            "71410898512698170152666726885200859146309071005450"//&
+            "6574372779462222353600257430609476112613376"
+        cond = c == a**p
 
-        print "(a)", "(d) power"
-        print "(a, a1, 50(i1))", "a         = ", a%sgn, a%arr
-        print "(a, i4)", "p         = ", p
-        print "(a, a1, 50(i1))", "c = a**p  = ", c%sgn, c%arr
+        call test(cond, "power")
     end block test_power
+
+contains
+
+    subroutine test(input_condition, input_name)
+        logical, intent(inout) :: input_condition
+        character(*), intent(in) :: input_name
+
+        if (input_condition) then
+            print "(a)", "Testing "//input_name//"... passed."
+        else
+            print "(a)", "Testing "//input_name//"... not passed!"
+            stop
+        end if
+    end subroutine test
+
+    subroutine title(input_name)
+        character(*), intent(in) :: input_name
+
+        print "(a)", "Testing "//input_name//"..."
+    end subroutine title
 
 end program main
--- test/test_euler_primes_m/test_euler_primes_m.f90
+++ test/test_euler_primes_m/test_euler_primes_m.f90
@@ -3,17 +3,42 @@ program main
     use euler_primes_m, only: sieve_of_Eratosthenes
     implicit none
 
+    call title("'euler_primes_m'")
     generate_arr_of_primes: block
         integer :: i
         logical, allocatable :: is_prime(:)
         integer, allocatable :: prime(:)
         integer, parameter :: PRIME_UPPER_BOUND = 100
+        integer, allocatable :: primes_under_100(:)
+        logical :: cond
 
+        primes_under_100 = [2,  3,  5,  7, 11, 13, 17, 19, 23, &
+                            29, 31, 37, 41, 43, 47, 53, 59, 61, &
+                            67, 71, 73, 79, 83, 89, 97]
         call sieve_of_Eratosthenes(PRIME_UPPER_BOUND, is_prime)
         prime = pack([(i, i=0, PRIME_UPPER_BOUND)], is_prime)
-        print "(a)", new_line("a")//"Test sieve_of_Eratosthenes"
-        print "(a, i5)", "Print primes less than: ", PRIME_UPPER_BOUND
-        print "(10(i3, ','))", prime
+        cond = all(primes_under_100 == prime)
+        call test(cond, "sieve_of_Eratosthenes")
     end block generate_arr_of_primes
+
+contains
+
+    subroutine test(input_condition, input_name)
+        logical, intent(inout) :: input_condition
+        character(*), intent(in) :: input_name
+
+        if (input_condition) then
+            print "(a)", "Testing "//input_name//"... passed."
+        else
+            print "(a)", "Testing "//input_name//"... not passed!"
+            stop
+        end if
+    end subroutine test
+
+    subroutine title(input_name)
+        character(*), intent(in) :: input_name
+
+        print "(a)", "Testing "//input_name//"..."
+    end subroutine title
 
 end program main
