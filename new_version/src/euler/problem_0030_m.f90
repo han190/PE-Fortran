5bfca4fc7245f3faaef93e5a7c87032a950e54ac
han190 <limpid.mazarine@gmail.com>
2021-12-04 00:21:16 -0900

    update for problem 30

---
+++ new_version/src/euler/problem_0030_m.f90
@@ -0,0 +1,20 @@
+submodule(interface_m) euler_problem_0030_m
+    implicit none
+
+contains
+
+    module pure character(len=20) function euler0030()
+        write (euler0030, "(i20)") answer()
+    end function euler0030
+
+    pure integer(i32) function answer()
+        integer(i32), parameter :: n = 999999
+        integer(i32) :: i
+
+        answer = 0
+        do i = 2, n
+            if (sum(to_array(i)**5) == i) answer = answer + i
+        end do
+    end function answer
+
+end submodule euler_problem_0030_m
