94b37be11e104c1a607a55d8a2863dd6cb08e3ff
han190 <limpid.mazarine@gmail.com>
2021-12-03 18:07:45 -0900

    update for problem 27; automate problem counting

--- new_version/problem_ID.py
+++ new_version/problem_ID.py
@@ -1,14 +1,27 @@
 import sys
+import os, os.path
 
-def integerArray(val):
-    longString = ''
+
+def count_files(dir):
+    def satisfied(x):
+        return os.path.isfile(os.path.join(dir, x))
+
+    return len([name for name in os.listdir(dir) if satisfied(name)])
+
+
+def integer_array(val):
+    longString = ""
     for i in range(1, val):
-        longString += str(i).zfill(4) + ' '
+        longString += str(i).zfill(4) + " "
     longString += str(val).zfill(4)
     return longString
 
+
 def main():
-    print(integerArray(int(str(sys.argv[1]))))
+    current_directory = "./src/euler/"
+    nfiles = count_files(current_directory)
+    print(integer_array(nfiles))
+
 
 if __name__ == "__main__":
     main()
---
+++ new_version/src/euler/problem_0027_m.f90
@@ -0,0 +1,24 @@
+submodule(interface_m) euler_problem_0027_m
+    implicit none
+
+contains
+
+    module pure character(len=20) function euler0027()
+        write (euler0027, "(i20)") answer()
+    end function euler0027
+
+    pure integer(i32) function answer()
+        answer = -(2*30 + 1)*quadratic_primes(1, 41, 30)
+    end function answer
+
+    pure integer(i32) function quadratic_primes(i, j, n)
+        integer, intent(in) :: i, j, n
+
+        if (abs(i) >= 1000 .or. abs(j) > 1000) then
+            quadratic_primes = 0
+        else
+            quadratic_primes = n**2 + i*n + j
+        end if
+    end function quadratic_primes
+
+end submodule euler_problem_0027_m
