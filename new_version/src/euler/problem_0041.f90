3db6753f9821502ea2a0d9e2a6194a14888795a6
han190 <limpid.mazarine@gmail.com>
2021-12-10 09:21:21 -0900

    update for problem 39 - 42

---
+++ new_version/src/euler/problem_0041.f90
@@ -0,0 +1,23 @@
+submodule(interface_m) euler_problem_0041_m
+    implicit none
+
+contains
+
+    module character(len=20) function euler0041()
+        write (euler0041, "(i20)") answer()
+    end function euler0041
+
+    pure integer(i32) function answer()
+        use prime_m, only: Sieve_of_Eratosthenes
+        implicit none
+
+        integer(i32), parameter :: n = 7654321
+        logical, allocatable :: is_prime(:)
+
+        call Sieve_of_Eratosthenes(n, is_prime)
+        do answer = n, 1, -1
+            if (is_prime(answer) .and. is_pandigital(answer)) exit
+        end do
+    end function answer
+
+end submodule euler_problem_0041_m
