c4ec97d19246c6d2b8118cc074ed027ae7e4787f
han190 <limpid.mazarine@gmail.com>
2021-12-30 21:30:10 -0900

    refactor problem 60

--- src/euler/problem_0060.f90
+++ src/euler/problem_0060.f90
@@ -7,7 +7,7 @@ contains
         write (euler0060, "(i20)") answer()
     end function euler0060
 
-    integer(i64) function answer()
+    pure integer(i64) function answer()
         use prime_m, only: Sieve_of_Eratosthenes
         implicit none
 
@@ -15,7 +15,7 @@ contains
         integer(i64) :: i, k, flags(2)
         logical, allocatable :: is_prime(:), concs(:, :)
         integer(i64), allocatable :: primes(:), idx(:)
-        logical :: avail
+        logical :: avail, checked, succeed
 
         allocate (is_prime(n(1)))
         call Sieve_of_Eratosthenes(n(1), is_prime)
@@ -32,8 +32,10 @@ contains
         concs = .true.
 
         do while (avail)
-            if (.not. checked_before(idx, concs, flags)) then
-                if (is_prime_pair(primes(idx), is_prime, flags)) exit
+            call checked_before(idx, concs, flags, checked)
+            if (.not. checked) then
+                call is_prime_pair(primes(idx), is_prime, flags, succeed)
+                if (succeed) exit
                 call label(flags, idx, concs)
             end if
             call next_combination(k, n(2), flags, idx, avail)
@@ -42,112 +44,77 @@ contains
         answer = sum(primes(idx))
     end function answer
 
-    function is_prime_pair(test_pair, isprime, flag) result(try_succeed)
+    pure subroutine is_prime_pair(pair, is_prime_, flag, succeed)
         use prime_m, only: is_prime
         implicit none
 
-        integer(i64), intent(in) :: test_pair(:)
-        logical, allocatable, intent(in) :: isprime(:)
+        integer(i64), intent(in) :: pair(:)
+        logical, intent(in) :: is_prime_(:)
         integer(i64), intent(out) :: flag(2)
-        integer(i64) :: k, n, idx(2), ab_arr(2), ba_arr(2)
-        logical :: avail, try_succeed
+        logical, intent(out) :: succeed
+        integer(i64) :: idx(2), i, arr(2)
+        logical :: avail
 
-        k = 2
-        n = size(test_pair)
-
-        try_succeed = .true.
         idx = [1, 2]
         avail = .true.
         flag = 0
+        succeed = .true.
 
         do while (avail)
-            ab_arr = test_pair(idx)
-            ba_arr = test_pair(idx(2:1:-1))
-            associate (ab => concatenate(ab_arr), ba => concatenate(ba_arr))
-                if (ab <= size(isprime)) then
-                    if (.not. isprime(ab)) then
-                        flag = idx
-                        try_succeed = .false.
-                        return
-                    end if
+            arr = [concatenate(pair(idx)), concatenate(pair(idx(2:1:-1)))]
+            do i = 1, 2
+                if (arr(i) <= size(is_prime_)) then
+                    succeed = is_prime_(arr(i))
                 else
-                    if (.not. is_prime(ab)) then
-                        flag = idx
-                        try_succeed = .false.
-                        return
-                    end if
+                    succeed = is_prime(arr(i))
                 end if
 
-                if (ba <= size(isprime)) then
-                    if (.not. isprime(ba)) then
-                        flag = idx(2:1:-1)
-                        try_succeed = .false.
-                        return
-                    end if
-                else
-                    if (.not. is_prime(ba)) then
-                        flag = idx(2:1:-1)
-                        try_succeed = .false.
-                        return
-                    end if
+                if (.not. succeed) then
+                    flag = idx
+                    return
                 end if
-                call permute(k, n, idx, avail)
-            end associate
+            end do
+            call permute(2_i64, size(pair, kind=i64), idx, avail)
         end do
-    end function
+    end subroutine is_prime_pair
 
-    function checked_before(test_idx, conc_is_prime, flags) result(ret)
-        integer(i64), intent(in) :: test_idx(:)
-        logical, intent(in) :: conc_is_prime(:, :)
+    pure subroutine checked_before(idx, concs, flags, ret)
+        integer(i64), intent(in) :: idx(:)
+        logical, intent(in) :: concs(:, :)
         integer(i64), intent(inout) :: flags(2)
-        logical :: ret, next_avail
-        integer(i64) :: k, n, idx(2)
+        logical, intent(out) :: ret
+        logical :: avail
+        integer(i64) :: i(2)
 
-        k = 2
-        n = size(test_idx)
-        idx = [1, 2]
-        ret = .false.
-        next_avail = .true.
-
-        do while (next_avail)
-            associate (x => test_idx(idx(1)), y => test_idx(idx(2)), &
-                       s => size(conc_is_prime(1, :)))
-                if (x < s .and. y < s) then
-                    if (.not. conc_is_prime(x, y)) then
-                        ret = .true.
-                        flags = idx
-                        return
-                    end if
+        i = [1, 2]; ret = .false.; avail = .true.
+        do while (avail)
+            associate (x => idx(i), s => size(concs(1, :)))
+                if (all(x < s) .and. .not. concs(x(1), x(2))) then
+                    ret = .true.; flags = i; return
                 end if
             end associate
-            call permute(k, n, idx, next_avail)
+            call permute(2_i64, size(idx, kind=i64), i, avail)
         end do
-    end function checked_before
+    end subroutine checked_before
 
     pure subroutine next_combination(k, n, flags, idx, ret)
         integer(i64), intent(in) :: k, n, flags(2)
         integer(i64), intent(inout) :: idx(:)
         logical, intent(out) :: ret
-        integer(i64) :: i, end_arr(k), flag
+        integer(i64) :: i
 
-        flag = max(flags(1), flags(2))
-        if (flag == k .or. flag == 0_i64) then
-            call permute(k, n, idx, ret)
-            return
+        if (all(idx == [(i, i=n - k + 1, n)])) then
+            ret = .false.; return
         end if
 
-        end_arr = [(i, i=n - k + 1, n)]
         ret = .true.
-        if (all(idx == end_arr)) then
-            ret = .false.
-            return
-        end if
-
-        if (idx(size(idx)) == n) then
-            call permute(k, n, idx, ret)
-        else
-            idx(flag:k) = [(idx(flag) + i, i=1, k - flag + 1)]
-        end if
+        associate (f => (max(flags(1), flags(2))))
+            if (f == k .or. f == 0 .or. idx(size(idx)) == n) then
+                call permute(k, n, idx, ret)
+            else
+                idx(f:k) = [(idx(f) + i, i=1, k - f + 1)]
+            end if
+        end associate
     end subroutine next_combination
 
     pure subroutine label(flags, idx, conc)
@@ -155,11 +122,9 @@ contains
         logical, intent(inout) :: conc(:, :)
 
         associate (x => (idx(flags(1))), y => (idx(flags(2))))
-            if (x >= size(conc(1, :)) .or. &
-                y >= size(conc(1, :))) return
+            if (all([x, y] >= size(conc(1, :)))) return
             if (conc(x, y)) then
-                conc(x, y) = .false.
-                conc(y, x) = .false.
+                conc(x, y) = .false.; conc(y, x) = .false.
             end if
         end associate
     end subroutine label
