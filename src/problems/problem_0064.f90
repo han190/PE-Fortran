a7f26c7573b333017f6ab585b961c70f7530b90f
han190 <limpid.mazarine@gmail.com>
2023-11-21 00:08:18 -0900

    update P64

---
+++ src/problems/problem_0064.f90
@@ -0,0 +1,44 @@
+submodule(module_problem) submodule_euler0064
+implicit none
+contains
+
+module subroutine euler0064(problem)
+  class(problem_type), intent(inout) :: problem
+  integer(int64), allocatable :: array(:)
+  integer(int64) :: i, p, sln
+
+  sln = 0
+  do i = 1, 10000
+    if (i - sqrt(i)**2 > 0) then
+      p = continued_fraction_period(i, 1000_int64)
+      sln = sln + merge(0, 1, mod(p, 2) == 0)
+    end if
+  end do
+  write (problem%answer, "(i0)") sln
+end subroutine euler0064
+
+pure function continued_fraction_period(n, num_sequence) result(period)
+  integer(int64), intent(in) :: n, num_sequence
+  integer(int64) :: period
+  integer(int64) :: sequence(3, num_sequence), iterator(3)
+  integer(int64) :: i, j, k, a0
+
+  a0 = sqrt(n)
+  iterator = [0_int64, 1_int64, a0]
+  period = 1
+  sequence(:, period) = iterator
+  
+  outer: do i = 2, num_sequence
+    iterator(1) = iterator(2)*iterator(3) - iterator(1)
+    iterator(2) = (n - iterator(1)*iterator(1))/iterator(2)
+    iterator(3) = (a0 + iterator(1))/iterator(2)
+    check_existence: do j = 1, period
+      if (all(iterator == sequence(:, j))) exit outer
+    end do check_existence
+    period = period + 1
+    sequence(:, period) = iterator
+  end do outer
+  period = period - 1
+end function continued_fraction_period
+
+end submodule submodule_euler0064
